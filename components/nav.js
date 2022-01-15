723dc8b8868a5477d36c749447228af003105e54
Chris Gilbert <chris@chrisgilbert.dev>
2022-01-15 16:07:00 +0000

    Add palces to stay link to nav

--- components/nav.js
+++ components/nav.js
@@ -1,33 +1,31 @@
-import Link from 'next/link'
+import Link from "next/link";
 
-import styles from "../styles/components/Nav.module.css"
+import styles from "../styles/components/Nav.module.css";
 
 export default function Nav() {
-    return (
-        <header className={styles.container}>
-            <nav>
-                <ul className={styles.navWrapper}>
-                    <li>
-                        <Link href="/places-to-visit">
-                            <a>Places to visit</a>
-                        </Link>
-                    </li>
-                    <li>
-                        Places to eat
-                    </li>
-                    <li className={styles.navTitle}>
-                        <Link href="/">
-                            <a>Provence 2022</a>
-                        </Link>
-                    </li>
-                    <li>
-                        Places to stay
-                    </li>
-                    <li>
-                        Other things to do
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+  return (
+    <header className={styles.container}>
+      <nav>
+        <ul className={styles.navWrapper}>
+          <li>
+            <Link href="/places-to-visit">
+              <a>Places to visit</a>
+            </Link>
+          </li>
+          <li>Places to eat</li>
+          <li className={styles.navTitle}>
+            <Link href="/">
+              <a>Provence 2022</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/places-to-stay">
+              <a>Places to stay</a>
+            </Link>
+          </li>
+          <li>Other things to do</li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
