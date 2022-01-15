bab15a0929f70f9b9f4409835ad966b9e7ff1f75
Chris Gilbert <chris@chrisgilbert.dev>
2022-01-15 14:38:13 +0000

    Style page header

--- components/pageHeader.js
+++ components/pageHeader.js
@@ -1,14 +1,11 @@
-import styles from "../styles/components/PageHeader.module.css"
+import styles from "../styles/components/PageHeader.module.css";
 
-export default function PageHeader() {
-    return (
-        <div className={styles.container}>
-            <span className={styles.topic}>Places to visit</span>
-            <h1 className={styles.h1}>Our favourite villages and towns in Provence</h1>
-            <p>
-                Provence is famous for its beautiful hillside villages.
-                From the quintesiaally xxx to the xxx here is a list of some of our favourite xxxxxx
-            </p>
-        </div>
-    )
+export default function PageHeader({ span, h1, p }) {
+  return (
+    <div className={styles.container}>
+      <span>{span}</span>
+      <h1>{h1}</h1>
+      <p>{p}</p>
+    </div>
+  );
 }
--- pages/places-to-visit.js
+++ pages/places-to-visit.js
@@ -11,7 +11,11 @@ export default function PlacesToVisit() {
     <>
       <HeroImage />
       <Container>
-        <PageHeader />
+        <PageHeader
+          span="Places to visit "
+          h1="Our favourite villages and towns in Provence"
+          p="Provence is famous for its beautiful hillside villages which are extremely picturesque and oozing with charm. From the quintessential Ménerbes to lazy long afternoons spent in Gordes here is a list of some of our favourite places to visit."
+        />
         <ListContainer>
           {content.map((c) => (
             <Recommendation key={c.title} {...c} />
