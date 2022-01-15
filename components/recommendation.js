6ecb56bf209e2efc30225d46fe17bc515d8125ec
Chris Gilbert <chris@chrisgilbert.dev>
2022-01-15 15:17:24 +0000

    More styling

--- components/recommendation.js
+++ components/recommendation.js
@@ -1,20 +1,22 @@
-import Image from "next/image"
+import Image from "next/image";
 
-import styles from "../styles/components/Recommendation.module.css"
+import styles from "../styles/components/Recommendation.module.css";
 
-export default function Recommendation({ title, content }) {
-    return (
-        <li className={styles.container}>
-            <div className={styles.imageContainer}>
-                <Image src="/placeholder-1.jpg"
-                    layout="fill"
-                    alt={title}
-                    objectFit="cover" />
-            </div>
-            <div className={styles.contentContainer}>
-                <h2>{title}</h2>
-                <p>{content}</p>
-            </div>
-        </li>
-    )
+export default function Recommendation({ title, content, image }) {
+  return (
+    <li className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image
+          src={`/images/${image}`}
+          layout="fill"
+          alt={title}
+          objectFit="cover"
+        />
+      </div>
+      <div className={styles.contentContainer}>
+        <h2>{title}</h2>
+        <p>{content}</p>
+      </div>
+    </li>
+  );
 }
