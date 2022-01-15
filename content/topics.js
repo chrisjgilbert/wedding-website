acbdfa56c444d7ecac8bb9fa458ec394f8f7617f
Chris Gilbert <chris@chrisgilbert.dev>
2022-01-15 14:21:59 +0000

    Update topics content

--- content/topics.js
+++ content/topics.js
@@ -1,22 +1,28 @@
-export default [
-    {
-        topic: "Places to visit",
-        title: "Our favourite villages and towns",
-        copy: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore."
-    },
-    {
-        topic: "Places to stay",
-        title: "Our favourite places to stay in Provence",
-        copy: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore."
-    },
-    {
-        topic: "Places to eat",
-        title: "Our favourite places to eat Provence",
-        copy: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore."
-    },
-    {
-        topic: "Other things to do",
-        title: "Our favourite things to do Provence",
-        copy: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore."
-    }
-]
+const topics = [
+  {
+    topic: "Places to visit",
+    title: "Our favourite villages and towns",
+    copy: "Provence is filled with many picturesque villages to discover and explore. From closeby Menerbes to further a field, there are many cute towns scattered across the Luberon valley to visit.",
+    image: "villages_towns.png",
+  },
+  {
+    topic: "Places to stay",
+    title: "Local Hotels and B&B’s",
+    copy: "Provence is filled with many picturesque villages to discover and explore. From closeby Menerbes to further a field, there are many cute towns scattered across the Luberon valley to visit.",
+    image: "hotels_bnbs.png",
+  },
+  {
+    topic: "Places to eat and drink",
+    title: "The Best Restaurants, Cafes & Rose Pit-Stops",
+    copy: "Provence is filled with many picturesque villages to discover and explore. From closeby Menerbes to further a field, there are many cute towns scattered across the Luberon valley to visit.",
+    image: "restaurants_cafes.png",
+  },
+  {
+    topic: "Other things to do in the area",
+    title: "Other Activicties to keep you busy",
+    copy: "Provence is filled with many picturesque villages to discover and explore. From closeby Menerbes to further a field, there are many cute towns scattered across the Luberon valley to visit.",
+    image: "other_activities.png",
+  },
+];
+
+export default topics;
